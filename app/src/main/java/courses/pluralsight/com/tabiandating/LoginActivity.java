2ffc9d04aa498a21a0159c833fb83e3efff6ffd4
Jigs <jhegs.knightdev@gmail.com>
2018-11-12 15:24:01 +0800

    added tag in LoginActivity

--- app/src/main/java/courses/pluralsight/com/tabiandating/LoginActivity.java
+++ app/src/main/java/courses/pluralsight/com/tabiandating/LoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "LoginActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
