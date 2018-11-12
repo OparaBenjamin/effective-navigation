9cedfbb79a62bb5bdb0ccc969bc6c683ea6332a3
Jigs <jhegs.knightdev@gmail.com>
2018-11-12 15:48:41 +0800

    added new MainActivity class

--- app/src/main/java/courses/pluralsight/com/tabiandating/LoginActivity.java
+++ app/src/main/java/courses/pluralsight/com/tabiandating/LoginActivity.java
@@ -1,14 +1,31 @@
 package courses.pluralsight.com.tabiandating;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "LoginActivity";
+
+    private Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Log.d(TAG, "onCreate: started");
+
+        mButton = findViewById(R.id.btn_login);
+        mButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_login){
+            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+        }
     }
 }
