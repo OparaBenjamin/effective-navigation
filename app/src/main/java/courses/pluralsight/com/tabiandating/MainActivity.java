f682e0f082a5ecae4176bcc1c24c85f0e963f412
Jigs <jhegs.knightdev@gmail.com>
2018-11-12 16:10:45 +0800

    added new class preference keys

--- app/src/main/java/courses/pluralsight/com/tabiandating/MainActivity.java
+++ app/src/main/java/courses/pluralsight/com/tabiandating/MainActivity.java
@@ -2,12 +2,19 @@ package courses.pluralsight.com.tabiandating;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    
+    private void isFirstLogin(){
+        Log.d(TAG, "isFirstLogin: checking if first login");
     }
 }
