643aead04339fd3d7957fb914eb7848bd5ebe184
Jigs <jhegs.knightdev@gmail.com>
2018-11-12 16:53:37 +0800

    added alert dialog for first time login

--- app/src/main/java/courses/pluralsight/com/tabiandating/MainActivity.java
+++ app/src/main/java/courses/pluralsight/com/tabiandating/MainActivity.java
@@ -1,7 +1,9 @@
 package courses.pluralsight.com.tabiandating;
 
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        isFirstLogin();
     }
     
     private void isFirstLogin(){
@@ -25,7 +29,23 @@ public class MainActivity extends AppCompatActivity {
         boolean isFirstLogin = sharedPreferences.getBoolean(PreferenceKeys.FIRST_TIME_LOGIN,true);
 
         if (isFirstLogin){
+            Log.d(TAG, "isFirstLogin: launching alert dialog");
 
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setMessage(getString(R.string.first_time_user_message));
+            alertDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean(PreferenceKeys.FIRST_TIME_LOGIN,false);
+                    editor.commit();
+                    dialog.dismiss();
+                }
+            });
+            alertDialog.setIcon(R.drawable.logo);
+            alertDialog.setTitle(" ");
+            alertDialog.create();
+            alertDialog.show();
         }
     }
 }
